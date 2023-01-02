f62ddc7ab7d8e891dfc2fe3ce880e2af15bcbfae
Nick Amell <namell91@gmail.com>
2023-01-02 16:24:58 -0600

    more linting

--- intellistop/libs/api.py
+++ intellistop/libs/api.py
@@ -1,10 +1,25 @@
-import yfinance as yf
+""" api.py
+
+Utilizes yfinance and organizes data in a more addressable way
+"""
 import pandas as pd
+import yfinance as yf
 
 from .lib_types import ConfigProperties
 
 
 def download_data(fund: str, config: ConfigProperties) -> dict:
+    """download_data
+
+    Function that does the actual data pull from yfinance for ticker info
+
+    Args:
+        fund (str): ticker string (e.g. "VTSAX", "AAPL", etc.)
+        config (ConfigProperties): period and date properties for fund, though default is best
+
+    Returns:
+        dict: formatted data in a dictionary: data['fund_name'] = {'Close': [], 'Date': [], ...}
+    """
     period = config.yf_properties.period
     interval = config.yf_properties.interval
     start_date = config.yf_properties.start_date
@@ -25,11 +40,22 @@ def download_data(fund: str, config: ConfigProperties) -> dict:
         data = yf.download(tickers=fund, start=start_date, interval=interval, group_by='ticker')
     else:
         data = yf.download(tickers=fund, period=period, interval=interval, group_by='ticker')
-    formatted_data = data_format(data, fund)
+    formatted_data = format_data(data, fund)
     return formatted_data
 
 
-def data_format(yf_data: pd.DataFrame, fund_name: str) -> dict:
+def format_data(yf_data: pd.DataFrame, fund_name: str) -> dict:
+    """format_data
+
+    Takes downloaded data and formats it in the preferred format for this tool
+
+    Args:
+        yf_data (pd.DataFrame): data downloaded from yfinance for a fund
+        fund_name (str): ticker name (e.g. "AAPL", etc.)
+
+    Returns:
+        dict: formatted data: data['fund_name'] = {'Close': [], 'Date': [], ...}
+    """
     corrected_data = {}
 
     if 'Close' in yf_data.columns:
@@ -49,11 +75,13 @@ def data_format(yf_data: pd.DataFrame, fund_name: str) -> dict:
                 corrected_data[col[0]] = {}
             corrected_data[col[0]][col[1]] = {}
 
+        # pylint: disable=consider-using-dict-items
         for fund_ticker in corrected_data:
             corrected_data[fund_ticker]["Date"] = [
                 date.strftime("%Y-%m-%d") for date in yf_data[fund_ticker].index]
             for column in corrected_data[fund_ticker]:
                 if column != "Date":
+                    # pylint: disable=unnecessary-comprehension
                     corrected_data[fund_ticker][column] = [
                         price for price in yf_data[fund_ticker][column].values]
 
--- intellistop/libs/filters.py
+++ intellistop/libs/filters.py
@@ -1,16 +1,43 @@
+""" filters.py
+
+Module that houses all applicable filtering and moving average functions
+"""
 import numpy as np
 
 
 def simple_moving_average_filter(data: list, filter_size: int = 50) -> list:
+    """simple_moving_average_filter
+
+    Args:
+        data (list): data array to be filtered
+        filter_size (int, optional): size of sma filter in indexes. Defaults to 50.
+
+    Returns:
+        list: filtered data
+    """
     filtered = [0.0] * len(data)
     for i in range(0, filter_size - 1):
         filtered[i] = data[i]
     for i in range(filter_size - 1, len(data)):
         filtered[i] = np.average(data[i - (filter_size - 1):i + 1])
     return filtered
-    
 
-def exponential_moving_average_filter(data: list, filter_size: int = 50, smoothing_coeff: float = 2.0) -> list:
+
+def exponential_moving_average_filter(data: list,
+                                      filter_size: int = 50,
+                                      smoothing_coeff: float = 2.0) -> list:
+    """exponential_moving_average_filter
+
+    Exponential MA is a tighter-to-signal dataset than the traditional simple moving average.
+
+    Args:
+        data (list): data array to be filtered
+        filter_size (int, optional): size of ema filter in indexes. Defaults to 50.
+        smoothing_coeff (float, optional): ema coefficient, K. Defaults to 2.0.
+
+    Returns:
+        list: filtered data
+    """
     filtered = [0.0] * len(data)
     coeff = smoothing_coeff / (float(filter_size) + 1.0)
     for i in range(0, filter_size - 1):
@@ -22,20 +49,47 @@ def exponential_moving_average_filter(data: list, filter_size: int = 50, smoothi
 
 
 def weighted_moving_average_filter(data: list, filter_size: int = 50) -> list:
+    """weighted_moving_average_filter
+
+    Filter where linearly the most emphasis is on the latest data point
+
+    Args:
+        data (list): data array to be filtered
+        filter_size (int, optional): size of wma filter in indexes. Defaults to 50.
+
+    Returns:
+        list: filtered data
+    """
     filtered = [0.0] * len(data)
     for i in range(0, filter_size - 1):
         filtered[i] = data[i]
     for i in range(filter_size - 1, len(data)):
-        sum = 0.0
+        sum_val = 0.0
         sum_div = 0.0
         for j in range(i - (filter_size-1), i+1):
-            sum += data[j] * (float(j) + 1.0)
+            sum_val += data[j] * (float(j) + 1.0)
             sum_div += float(j) + 1.0
-        filtered[i] = sum / sum_div
+        filtered[i] = sum_val / sum_div
     return filtered
 
 
 def smart_moving_average(data: list, filter_size: int = 50) -> list:
+    """smart_moving_average
+
+    A filter that takes the combination of simple, exponential, and weighted moving averages
+    to generate a deeper moving average that combines traditional, typical market behavior (using
+    the simple moving average) with something that reacts more quickly to changes (the
+    exponential and weighted moving averages).
+
+    If sized correctly, this should become a major support line for the trend of a price data set.
+
+    Args:
+        data (list): data array to be filtered
+        filter_size (int, optional): size of the filter. Defaults to 50.
+
+    Returns:
+        list: filtered data
+    """
     sma = simple_moving_average_filter(data, filter_size=filter_size)
     ema = exponential_moving_average_filter(data, filter_size=filter_size)
     wma = weighted_moving_average_filter(data, filter_size=filter_size)
@@ -44,6 +98,16 @@ def smart_moving_average(data: list, filter_size: int = 50) -> list:
 
 
 def get_slope_of_data_set(data_set: list) -> list:
+    """get_slope_of_data_set
+
+    A simple differentiation function that returns the change in data from index to index.
+
+    Args:
+        data_set (list): data for which to find the slope data set
+
+    Returns:
+        list: slope data set of the original data set
+    """
     slope = [0.0] * len(data_set)
     for i in range(1, len(data_set)):
         slope[i] = data_set[i] - data_set[i-1]
