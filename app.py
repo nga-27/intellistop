2429a2fd9eff087721a500c781f7e89bd2279b6d
Nick Amell <namell91@gmail.com>
2023-07-19 16:39:36 -0500

    lists

--- app.py
+++ app.py
@@ -16,91 +16,95 @@ def run_app():
     startup.logo_renderer()
     startup.start_header()
     print("")
-    fund = input("Enter a fund ticker symbol: ").upper()
+    fund_raw = input("Enter a fund ticker [or tickers separated by a space]: ").upper()
     print("")
 
-    if len(fund) == 0:
+    if len(fund_raw) == 0:
         print("ERROR: No fund ticker entered on input. Exiting...")
         return
+    
+    fund_stripped = fund_raw.strip()
+    fund_list = fund_stripped.split(' ')
 
     stops = IntelliStop()
-    print(f"Starting 'Intellistop' with fund ticker '{fund}'...")
+    print(f"Starting 'Intellistop' with fund ticker(s): '{fund_raw}'...")
 
-    vf_data = stops.run_analysis_for_ticker(fund)
-    close = stops.return_data(fund)
-    dates = stops.return_data(fund, key='__full__').get('Date', [])
+    for fund in fund_list:
+        vf_data = stops.run_analysis_for_ticker(fund)
+        close = stops.return_data(fund)
+        dates = stops.return_data(fund, key='__full__').get('Date', [])
 
-    green_zones = [vf_obj.time_index_list for vf_obj in vf_data.data_sets]
-    temp_concat = []
-    for array in green_zones:
-        temp_concat.extend(array)
-    red_zones = []
-    red_one = []
-    for i in range(len(close)):
-        if i not in temp_concat:
-            red_one.append(i)
-        else:
-            if len(red_one) > 0:
-                red_zones.append(red_one)
-                red_one = []
-    if len(red_one) > 0:
-        red_zones.append(red_one)
-
-    yellow_zones = []
-    for vf_obj in vf_data.data_sets:
-        yellow_one = []
-        for i, ind in enumerate(vf_obj.time_index_list):
-            if close[ind] < vf_obj.caution_line[i]:
-                yellow_one.append(ind)
+        green_zones = [vf_obj.time_index_list for vf_obj in vf_data.data_sets]
+        temp_concat = []
+        for array in green_zones:
+            temp_concat.extend(array)
+        red_zones = []
+        red_one = []
+        for i in range(len(close)):
+            if i not in temp_concat:
+                red_one.append(i)
             else:
-                if len(yellow_one) > 0:
-                    yellow_zones.append(yellow_one)
-                    yellow_one = []
-    if len(yellow_one) > 0:
-        yellow_zones.append(yellow_one)
+                if len(red_one) > 0:
+                    red_zones.append(red_one)
+                    red_one = []
+        if len(red_one) > 0:
+            red_zones.append(red_one)
 
-    min_value = min(
-        [
-            min(min(vf_obj.stop_loss_line) for vf_obj in vf_data.data_sets),
-            min(close)
-        ]
-    )
-    range_value = max(close) - min_value
+        yellow_zones = []
+        for vf_obj in vf_data.data_sets:
+            yellow_one = []
+            for i, ind in enumerate(vf_obj.time_index_list):
+                if close[ind] < vf_obj.caution_line[i]:
+                    yellow_one.append(ind)
+                else:
+                    if len(yellow_one) > 0:
+                        yellow_zones.append(yellow_one)
+                        yellow_one = []
+        if len(yellow_one) > 0:
+            yellow_zones.append(yellow_one)
 
-    status_string = f"{fund} is currently in a green zone. BUY."
-    status_color = 'green'
-    if vf_data.current_status.status.value == 'stopped_out':
-        status_string = f"{fund} is currently STOPPED OUT. SELL / wait for a re-entry signal."
-        status_color = 'red'
-    elif vf_data.current_status.status.value == 'caution_zone':
-        status_string = f"{fund} is currently in a caution state. HOLD."
-        status_color = 'yellow'
+        min_value = min(
+            [
+                min(min(vf_obj.stop_loss_line) for vf_obj in vf_data.data_sets),
+                min(close)
+            ]
+        )
+        range_value = max(close) - min_value
 
-    shown_stop_loss = f"VF: {np.round(vf_data.vf.curated, 3)}\n"
-    if vf_data.current_status.status.value != 'stopped_out':
-        shown_stop_loss += f"Stop Loss: ${np.round(vf_data.stop_loss.curated, 2)}"
-    else:
-        shown_stop_loss += "Stop Loss: n/a"
+        status_string = f"{fund} is currently in a green zone. BUY."
+        status_color = 'green'
+        if vf_data.current_status.status.value == 'stopped_out':
+            status_string = f"{fund} is currently STOPPED OUT. SELL / wait for a re-entry signal."
+            status_color = 'red'
+        elif vf_data.current_status.status.value == 'caution_zone':
+            status_string = f"{fund} is currently in a caution state. HOLD."
+            status_color = 'yellow'
 
-    plot_config = plot.set_plot_config(
-        f"{fund}_stop_losses.png",
-        f"{fund} - Stop Loss Analysis",
-        vf_stop_loss_text=shown_stop_loss,
-        view=True
-    )
-    plot.app_plot(
-        close,
-        dates,
-        vf_data.data_sets,
-        green_zones,
-        red_zones,
-        yellow_zones,
-        range_value,
-        min_value,
-        config=plot_config,
-        text_str=status_string,
-        str_color=status_color
-    )
+        shown_stop_loss = f"VF: {np.round(vf_data.vf.curated, 3)}\n"
+        if vf_data.current_status.status.value != 'stopped_out':
+            shown_stop_loss += f"Stop Loss: ${np.round(vf_data.stop_loss.curated, 2)}"
+        else:
+            shown_stop_loss += "Stop Loss: n/a"
+
+        plot_config = plot.set_plot_config(
+            f"{fund}_stop_losses.png",
+            f"{fund} - Stop Loss Analysis",
+            vf_stop_loss_text=shown_stop_loss,
+            view=True
+        )
+        plot.app_plot(
+            close,
+            dates,
+            vf_data.data_sets,
+            green_zones,
+            red_zones,
+            yellow_zones,
+            range_value,
+            min_value,
+            config=plot_config,
+            text_str=status_string,
+            str_color=status_color
+        )
 
 
 if __name__ == "__main__":
