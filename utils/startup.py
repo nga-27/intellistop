e66b1a10451e57123424abab6df0a02d374b96f2
Nick Amell <namell91@gmail.com>
2026-05-09 10:42:52 -0500

    start of some upgrades

--- utils/startup.py
+++ utils/startup.py
@@ -4,7 +4,7 @@ Functions for app prints on running script
 """
 from pathlib import Path
 import time
-from typing import Tuple
+from typing import Tuple, List
 from importlib.metadata import version
 
 
@@ -56,7 +56,7 @@ def logo_renderer():
         time.sleep(1)
 
 
-def handle_startup() -> Tuple[list, str, bool]:
+def handle_startup() -> Tuple[list, str, bool, bool]:
     """handle_startup
 
     returns the fund list content
@@ -68,13 +68,36 @@ def handle_startup() -> Tuple[list, str, bool]:
     logo_renderer()
     start_header()
     print("")
-    fund_raw = input("Enter a fund ticker [or tickers separated by a space]: ").upper()
+    fund_raw = input("Enter a fund ticker [or tickers separated by a space or comma]: ").upper()
     print("")
 
     if len(fund_raw) == 0:
         print("ERROR: No fund ticker entered on input. Exiting...")
-        return [], "", True
+        return [], "", True, True
 
+    should_plot = True
+    fund_list: List[str] = []
     fund_stripped = fund_raw.strip()
-    fund_list = fund_stripped.split(' ')
-    return fund_list, fund_raw, False
+    commas = fund_stripped.split(',')
+    for fund in commas:
+        space_split = fund.split(' ')
+        for item in space_split:
+            if len(item) > 0:
+                fund_list.append(item.strip())
+    returnable_fund_list = []
+    for fund in fund_list:
+        if "--" in fund:
+            cmd = fund.replace("--", "")
+            if cmd == "NO_PLOT":
+                should_plot = False
+            else:
+                print(f"ERROR: Unrecognized command '{cmd}' in input. Exiting...")
+                return [], "", True, True
+        else:
+            returnable_fund_list.append(fund)
+    returnable_fund_set = set(returnable_fund_list)
+    if len(returnable_fund_set) == 0:
+        print("ERROR: No valid fund tickers entered on input. Exiting...")
+        return [], "", True, True
+    returnable_fund_list = list(returnable_fund_set)
+    return returnable_fund_list, fund_raw, False, should_plot
